2b7bc442dad663968772a089716292fd37f46907
Wisfie <wiz.fie@gmail.com>
2024-01-05 01:50:21 +0700

    cors

--- src/main/java/example/penilaian/CorsConfig.java
+++ src/main/java/example/penilaian/CorsConfig.java
@@ -10,12 +10,12 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("https://penilaian-ms.vercel.app")
 //                .allowedOrigins("https://penilaian-logistic.vercel.app/")
-                .allowedOrigins("https://penilaian-ms.vercel.app/")
-                .allowedOrigins("http://localhost:5173")
-                .allowedOrigins("http://192.168.43.176:5173/")
-                .allowedOrigins("http://192.168.1.11:5173/")
-                .allowedOrigins("http://192.168.1.5:5173/")
+//                .allowedOrigins("http://localhost:5173")
+//                .allowedOrigins("http://192.168.43.176:5173/")
+//                .allowedOrigins("http://192.168.1.11:5173/")
+//                .allowedOrigins("http://192.168.1.5:5173/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Content-Type", "Authorization");
     }
